832b0be87dc311b91cdf13a350fbe34343090ddb
Nathan Logan <nathanlogan22@gmail.com>
2020-10-21 10:48:17 +1000

    Remove outdated keys

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# set SPOTIPY_CLIENT_ID=66788a5caa9c4e408c92827077e4e7e2
-# set SPOTIPY_CLIENT_SECRET=e996f4ccde12449baa146590b823bac4
-# set SPOTIPY_REDIRECT_URI=https://nathan-logan.github.io/spotify-overlay/callback
-
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import tkinter as tk
